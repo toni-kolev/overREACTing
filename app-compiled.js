12dfd91559eb76e5659f75ce1f085af2a88d5aa4
Toni Kolev <kolev@toni-kolev.com>
2018-07-04 12:36:58 +0300

    Some more changes

--- app-compiled.js
+++ app-compiled.js
@@ -12,7 +12,7 @@ var myDiv = React.createElement(
 
 ReactDOM.render(myDiv, document.getElementById('el1'));
 
-/////////////
+////////////////////////////////////////////////////////////////////////
 
 ReactDOM.render(React.createElement(
   'h1',
@@ -20,7 +20,7 @@ ReactDOM.render(React.createElement(
   2 + 3
 ), document.getElementById('el2'));
 
-////////////
+////////////////////////////////////////////////////////////////////////
 
 const theBestString = 'tralalalala i am da best';
 
@@ -30,7 +30,7 @@ ReactDOM.render(React.createElement(
   theBestString
 ), document.getElementById('el3'));
 
-///////
+////////////////////////////////////////////////////////////////////////
 
 var goose = 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-goose.jpg';
 
@@ -38,7 +38,7 @@ var gooseImg = React.createElement('img', { src: goose });
 
 ReactDOM.render(gooseImg, document.getElementById('el4'));
 
-/////////////////////
+////////////////////////////////////////////////////////////////////////
 
 function makeDoggy(e) {
   // Call this extremely useful function on an <img>.
@@ -54,3 +54,80 @@ const kitty = React.createElement('img', {
   alt: 'kitty' });
 
 ReactDOM.render(kitty, document.getElementById('el5'));
+
+////////////////////////////////////////////////////////////////////////
+function coinToss() {
+  // This function will randomly return either 'heads' or 'tails'.
+  return Math.random() < 0.5 ? 'heads' : 'tails';
+}
+
+const pics = {
+  kitty: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-kitty.jpg',
+  doggy: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-puppy.jpeg'
+};
+let img;
+
+// if/else statement begins here:
+if (coinToss() === 'heads') {
+  img = React.createElement('img', { src: pics.kitty, width: '100px' });
+} else {
+  img = React.createElement('img', { src: pics.doggy, width: '100px' });
+}
+
+ReactDOM.render(img, document.getElementById('el6'));
+
+////////////////////////////////////////////////////////////////////////
+
+const img2 = React.createElement('img', { src: pics[coinToss() === 'heads' ? 'kitty' : 'doggy'], width: '125px' });
+
+ReactDOM.render(img2, document.getElementById('el7'));
+
+////////////////////////////////////////////////////////////////////////
+const people = ['Rowe', 'Prevost', 'Gare'];
+
+const peopleLis = people.map(person =>
+// expression goes here:
+React.createElement(
+  'li',
+  null,
+  person
+));
+
+// ReactDOM.render goes here:
+ReactDOM.render(React.createElement(
+  'ul',
+  null,
+  peopleLis
+), document.getElementById('el8'));
+
+////////////////////////////////////////////////////////////////////////
+
+const peopleLis2 = people.map((person, i) =>
+// expression goes here:
+React.createElement(
+  'li',
+  { key: 'person_' + i },
+  person
+));
+
+// ReactDOM.render goes here:
+ReactDOM.render(React.createElement(
+  'ul',
+  null,
+  peopleLis2
+), document.getElementById('el9'));
+
+////////////////////////////////////////////////////////////////////////
+class ToniComponent extends React.Component {
+  render() {
+    return React.createElement(
+      'h1',
+      null,
+      'Hello world'
+    );
+  }
+};
+
+React.createElement(ToniComponent, null);
+
+ReactDOM.render(React.createElement(ToniComponent, null), document.getElementById("el10"));
--- app.js
+++ app.js
@@ -6,21 +6,21 @@ var myDiv = (
   
 ReactDOM.render(myDiv, document.getElementById('el1'));
 
-/////////////
+////////////////////////////////////////////////////////////////////////
 
 ReactDOM.render(
     <h1>{2 + 3}</h1>,
     document.getElementById('el2')
   );
 
-////////////
+////////////////////////////////////////////////////////////////////////
 
   const theBestString = 'tralalalala i am da best';
   
   ReactDOM.render(<h1>{theBestString}</h1>, document.getElementById('el3'));
 
 
-///////
+////////////////////////////////////////////////////////////////////////
 
   var goose = 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-goose.jpg';
   
@@ -30,7 +30,7 @@ ReactDOM.render(
   
   ReactDOM.render(gooseImg, document.getElementById('el4'));
 
-/////////////////////
+////////////////////////////////////////////////////////////////////////
 
 function makeDoggy(e) {
     // Call this extremely useful function on an <img>.
@@ -48,3 +48,71 @@ function makeDoggy(e) {
   );
   
   ReactDOM.render(kitty, document.getElementById('el5'));
+
+////////////////////////////////////////////////////////////////////////
+  function coinToss() {
+    // This function will randomly return either 'heads' or 'tails'.
+    return Math.random() < 0.5 ? 'heads' : 'tails';
+  }
+  
+  const pics = {
+    kitty: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-kitty.jpg',
+    doggy: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-puppy.jpeg'
+  };
+  let img;
+  
+  // if/else statement begins here:
+  if (coinToss() === 'heads') {
+    img = <img src={pics.kitty} width="100px"/>;
+  } else {
+    img = <img src={pics.doggy} width="100px" />;
+  }
+  
+  ReactDOM.render(img, document.getElementById('el6'));
+
+
+////////////////////////////////////////////////////////////////////////
+  
+const img2 = <img src={pics[coinToss() === 'heads' ? 'kitty' : 'doggy']} width="125px"/>;
+  
+  ReactDOM.render(
+      img2, 
+      document.getElementById('el7')
+  );
+
+////////////////////////////////////////////////////////////////////////
+const people = ['Rowe', 'Prevost', 'Gare'];
+
+const peopleLis = people.map(person =>
+  // expression goes here:
+	<li>{person}</li>
+);
+
+// ReactDOM.render goes here:
+ReactDOM.render(<ul>{peopleLis}</ul>,document.getElementById('el8'));
+
+
+////////////////////////////////////////////////////////////////////////
+
+const peopleLis2 = people.map((person, i) =>
+  // expression goes here:
+  <li key={'person_' + i}>{person}</li>
+);
+
+// ReactDOM.render goes here:
+ReactDOM.render(<ul>{peopleLis2}</ul>, document.getElementById('el9'));
+
+
+////////////////////////////////////////////////////////////////////////
+class ToniComponent extends React.Component {
+  render() {
+    return <h1>Hello world</h1>
+  };
+};
+
+<ToniComponent />
+
+ReactDOM.render(
+  <ToniComponent />,
+  document.getElementById("el10")
+);
